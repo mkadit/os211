fabc8557f5c2c064a47db8958ec2d563942ad315
mkadit <mkaditya00@gmail.com>
2021-03-09 11:59:12 +0700

    Change myscript.sh

--- TXT/myscript.sh
+++ TXT/myscript.sh
@@ -1,34 +1,173 @@
 #!/bin/bash
-# Cicak bin Kadal
-# Tue 13 Oct 2020 10:37:14 AM WIB
 
-FILES="my*.txt my*.sh"
-SHA="SHA256SUM"
+RC='
+"___  ___ _   __  ___      _ _ _
+"|  \/  || | / / / _ \    | (_) |
+"| .  . || |/ / / /_\ \ __| |_| |_
+"| |\/| ||    \ |  _  |/ _` | | __|
+"| |  | || |\  \| | | | (_| | | |_
+"\_|  |_/\_| \_/\_| |_/\__,_|_|\__|
 
-# Remove SHA256SUM and SHA256SUM.asc
-echo "rm -f $SHA $SHA.asc"
-rm -f $SHA $SHA.asc
+" change leader key
+let g:mapleader=" "
 
-# Print SHA256 all my*.txt and my*.sh to SHA256SUM
-echo "sha256sum $FILES > $SHA"
-sha256sum $FILES > $SHA
+""""""""""""""""""""""""""""""""""""""""""""""
+" General Settings
+""""""""""""""""""""""""""""""""""""""""""""""
+syntax enable
+set nocompatible
+set t_ut=
+set ttyfast
+set lazyredraw
+set noerrorbells
+set visualbell
+set t_vb=
+set laststatus=2
+set nocursorline
+set nobackup
+set nowritebackup
+set cmdheight=2
+set updatetime=300
+set shortmess+=c
+set signcolumn=yes
+set encoding=utf-8
+set hidden
+set splitbelow splitright
 
-# Check SHA256
-echo "sha256sum -c $SHA"
-sha256sum -c $SHA
+" set defautl tabs to have 4 spaces
+set sw=4 ts=4 sts=4 expandtab smarttab autoindent
+" show the matching part of the pair for [] {} and ()
+set showmatch
+"set colorcolumn=80
 
-# Create an output for SHA256 to SHA256.sum with armored (binary-to-textual) 
-# and detached sign it
-echo "gpg -o $SHA.asc -a -sb $SHA"
-gpg -o $SHA.asc -a -sb $SHA
+" Set the minimal number of lines under the cursor
+set scrolloff=2
 
-# Verify the SHA256SUM.asc with the signed data in SHA256SUM
-echo "gpg --verify $SHA.asc $SHA"
-gpg --verify $SHA.asc $SHA
+" Set so vim search (/ or ?) be case insensitive
+set ignorecase smartcase
 
-# Exit as the program succeed
+" Set hybrd line number
+set number relativenumber
+
+" Enable autocomplete
+set wildmenu
+set wildmode=longest:full,full
+
+" Set new split below or right
+set splitbelow splitright
+
+" ignore autocomplete *.class files
+set wildignore=*.class
+
+" navigate with mouse
+set mouse=a
+
+" show special characters
+set list
+set listchars=tab:›\ ,nbsp:␣,trail:•,extends:»,precedes:«
+set fillchars+=vert:│
+
+" highlight all search matches
+set hlsearch
+set incsearch
+
+" Show tab bar
+set showtabline=2
+
+set noshowmode
+" tell vim where to put swap files
+set dir=~/.swapdir
+set directory^=$HOME/.vim/swap//
+set backupdir=/tmp//
+set directory=/tmp//
+set undodir=/tmp//
+" set spellfile=~/.config/nvim/spell/en.utf-8.add
+
+
+""""""""""""""""""""""""""""""""""""""""""""""
+" System Mapping
+""""""""""""""""""""""""""""""""""""""""""""""
+" change indent in visual mode
+vnoremap > >gv
+vnoremap < <gv
+
+" file binding
+" noremap <S-z><S-a> :wa<CR>
+" noremap <C-q> :q<CR>
+
+" split binding
+noremap <C-S-h> gT
+noremap <C-S-l> gt
+
+noremap <C-h> <C-w>h
+noremap <C-j> <C-w>j
+noremap <C-k> <C-w>k
+noremap <C-l> <C-w>l
+
+" resize splits with arrow keys
+noremap <C-A-Left>	:vertical:resize -1<CR>
+noremap <C-A-Down>	:resize +1<CR>
+noremap <C-A-Up>	:resize -1<CR>
+noremap <C-A-Right>	:vertical:resize +1<CR>
+
+" Close buffer
+nnoremap <Leader>bd :bd<CR>
+
+" reload vimrc
+nnoremap <Leader>rr :so $MYVIMRC<CR>
+" Load Config file
+nnoremap <Leader>re :e $MYVIMRC<CR>
+
+" hide search results
+map <Esc><Esc> :nohlsearch<CR>
+
+
+" Buffers and Tabs
+nnoremap <C-A-j> :bprevious<CR>
+nnoremap <C-A-k> :bnext<CR>
+nnoremap <C-n> :bprevious<CR>
+nnoremap <C-m> :bnext<CR>
+nnoremap <C-A-h> :tabprevious<CR>
+nnoremap <C-A-l> :tabnext<CR>
+
+""""""""""""""""""""""""""""""""""""""""""""""
+" Configs
+""""""""""""""""""""""""""""""""""""""""""""""
+
+augroup autoSaveFolds
+  autocmd!
+  autocmd BufWinLeave * silent! mkview
+  autocmd BufWinEnter * silent! loadview
+augroup END
+
+augroup goodbye_netrw
+  au!
+  autocmd VimEnter * silent! au! FileExplorer *
+augroup END
+
+inoremap <C-v> <ESC>"*p
+vnoremap <C-c> "+y
+vnoremap <C-d> "+d
+'
+
+VIMR="$HOME/.vimrc"
+MESSAGE="Finish writing vim settings in $VIMR, enjoy!"
+
+writeVIMRC() {
+cat <<EOF > $VIMR
+$RC
+EOF
+}
+
+query() {
+    echo -n "Do you wish to override? [y/N] "
+    read ANSWER
+        ( [ -z "$ANSWER" ] && echo "Answer something please" ) || 
+        ( [ "$ANSWER" == "y" ] && rm $VIMR && writeVIMRC && echo "Your vim settings has changed!" ) ||
+        (  [ "$ANSWER" == "n" ] && echo "Your vim settings stay the same!" ) ||
+        ( echo "Answer something legit please" )
+
+}
+echo "Creating mkadit minimal vimrc"
+( [ ! -f $VIMR ]  && writeVIMRC && echo $MESSAGE ) || (query)
 exit 0
-
-# Mon Sep 28 21:05:04 WIB 2020
-# Tue 29 Sep 2020 11:02:39 AM WIB
-
